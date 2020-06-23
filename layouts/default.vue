29e13d45ea891025f31091dd681e3b8c555bd01b
Basti Lavarias <denzilbao@gmail.com>
2020-06-23 16:58:42 +0800

    [PROGRESS] ProfileSideBar.vue created.

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <profile-sidebar></profile-sidebar>
+          </v-col>
+        </v-row>
+        <v-col cols="12" md="8">
+          <nuxt></nuxt>
+        </v-col>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ProfileSidebar from '../components/ProfileSidebar'
+export default {
+  components: { ProfileSidebar },
+}
+</script>
