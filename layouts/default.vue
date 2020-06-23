f5e219e24569658d7689f6a314cc6fc44cdd4c86
Basti Lavarias <denzilbao@gmail.com>
2020-06-24 00:12:36 +0800

    [PROGRESS] ProjectCard.vue created.

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,30 @@
           <v-col cols="12" md="4">
             <profile-sidebar></profile-sidebar>
           </v-col>
+          <v-col cols="12" md="8">
+            <v-card class="mb-5">
+              <v-tabs v-model="tab" color="error" show-arrows>
+                <v-tab v-for="(tab, index) in tabs" :key="index">
+                  <v-icon class="mr-1">
+                    {{ tab.icon }}
+                  </v-icon>
+                  <span class="text-capitalize font-weight-bold">{{
+                    tab.text
+                  }}</span>
+                </v-tab>
+                <v-tab>
+                  <v-icon class="mr-1">
+                    mdi-file-account
+                  </v-icon>
+                  <span class="text-capitalize font-weight-bold"
+                    >Download my Resume</span
+                  >
+                </v-tab>
+              </v-tabs>
+            </v-card>
+            <nuxt></nuxt>
+          </v-col>
         </v-row>
-        <v-col cols="12" md="8">
-          <nuxt></nuxt>
-        </v-col>
       </v-container>
     </v-main>
   </v-app>
@@ -19,5 +39,24 @@
 import ProfileSidebar from '../components/ProfileSidebar'
 export default {
   components: { ProfileSidebar },
+
+  data() {
+    return {
+      tab: null,
+      tabs: [
+        {
+          text: 'About Me',
+          icon: 'mdi-information-variant',
+          to: { name: 'index' },
+        },
+
+        {
+          text: 'Blogs',
+          icon: 'mdi-blogger',
+          to: { name: 'index' },
+        },
+      ],
+    }
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,59 @@
-<template> </template>
+<template>
+  <v-card>
+    <v-card-title>Hello 🙋‍♂</v-card-title>
+    <v-card-text>
+      <span v-html="aboutMe.introduction"></span>
+    </v-card-text>
+    <v-card-title>
+      My Skills 👨‍💻️
+    </v-card-title>
+    <v-card-text>
+      <v-chip
+        v-for="(skill, index) in aboutMe.skills"
+        :key="index"
+        class="ma-2"
+      >
+        <v-icon class="mr-1">{{ skill.icon }}</v-icon>
+        <span>{{ skill.text }}</span>
+      </v-chip>
+    </v-card-text>
+    <v-card-title>
+      Featured Projects 🖥️
+    </v-card-title>
+    <v-card-text>
+      <v-row dense>
+        <v-col
+          v-for="(project, index) in aboutMe.featuredProjects"
+          :key="index"
+          cols="12"
+          sm="6"
+          lg="4"
+        >
+          <project-card
+            :key="index"
+            :name="project.name"
+            :description="project.description"
+            :url="project.url"
+            :technologies="project.technologies"
+            :role="project.role"
+            :imagePaths="project.imagePaths"
+          ></project-card>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
 
 <script>
-export default {}
+import aboutMeData from '~/static/data/about-me.json'
+import ProjectCard from '../components/ProjectCard'
+
+export default {
+  components: { ProjectCard },
+  data() {
+    return {
+      aboutMe: aboutMeData,
+    }
+  },
+}
 </script>
