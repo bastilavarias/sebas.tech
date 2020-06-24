53ae92dea81b2cc91fa79733952ec4d78ab3cf6e
Basti Lavarias <denzilbao@gmail.com>
2020-06-24 18:20:44 +0800

    [PROGRESS] initial blogs function created.

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -3,8 +3,15 @@
     <v-card-title>My Blogs 📓</v-card-title>
     <v-card-text>
       <v-row>
-        <v-col cols="12" sm="6" xl="4" v-for="n in 1">
-          <blog-card :key="n"></blog-card>
+        <v-col cols="12" sm="6" xl="4" v-for="(blog, index) in blogs">
+          <blog-card
+            :key="index"
+            :title="blog.title"
+            :description="blog.description"
+            :language="blog.language"
+            :created-at="blog.createdAt"
+            :updated-at="blog.updatedAt"
+          ></blog-card>
         </v-col>
       </v-row>
     </v-card-text>
@@ -12,7 +19,15 @@
 </template>
 <script>
 import BlogCard from '../components/BlogCard'
+import blogsData from '../static/data/blogs.json'
+
 export default {
   components: { BlogCard },
+
+  data() {
+    return {
+      blogs: blogsData,
+    }
+  },
 }
 </script>
