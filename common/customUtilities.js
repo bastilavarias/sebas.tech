8af30b022460ae141ed8dc9e760458bd3831cdf5
Basti Lavarias <denzilbao@gmail.com>
2020-06-26 01:05:35 +0800

    gridsome setup and created index route.

--- common/customUtilities.js
+++
@@ -1,8 +0,0 @@
-export default {
-  methods: {
-    truncateText(string) {
-      const truncatedString = string.slice(0, 65)
-      return truncatedString ? `${truncatedString}...` : 'Something went wrong.'
-    },
-  },
-}
---
+++ src/pages/Index.vue
@@ -0,0 +1,96 @@
+<template>
+  <default-layout>
+    <v-card>
+      <v-card-title>Hello 🙋‍♂</v-card-title>
+      <v-card-text>
+        <span v-html="aboutMe.introduction"></span>
+      </v-card-text>
+      <v-card-title>
+        My Skills 👨‍💻️
+      </v-card-title>
+      <v-card-text>
+        <v-chip
+          v-for="(skill, index) in aboutMe.skills"
+          :key="index"
+          class="ma-2"
+        >
+          <v-icon class="mr-1">{{ skill.icon }}</v-icon>
+          <span>{{ skill.text }}</span>
+        </v-chip>
+      </v-card-text>
+      <v-card-title>
+        Featured Projects 🖥️
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col
+            v-for="(project, index) in aboutMe.featuredProjects"
+            :key="index"
+            cols="12"
+            sm="6"
+          >
+            <project-card
+              :key="index"
+              :name="project.name"
+              :description="project.description"
+              :url="project.url"
+              :technologies="project.technologies"
+              :role="project.role"
+              :imagePaths="project.imagePaths"
+            ></project-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-title>
+        Repositories 📁
+      </v-card-title>
+      <v-card-text>
+        <v-slide-group v-model="slidGroupState" show-arrows>
+          <v-slide-item v-for="(repository, index) in aboutMe.repositories">
+            <repository-card
+              :key="index"
+              :name="repository.name"
+              :description="repository.description"
+              :sourceCodeUrl="repository.sourceCodeUrl"
+              class-name="ma-1"
+            ></repository-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-card-text>
+      <v-card-actions>
+        <div class="flex-grow-1"></div>
+        <v-btn
+          text
+          color="error"
+          href="https://github.com/bastilavarias?tab=repositories"
+          target="_blank"
+        >
+          <span class="text-capitalize mr-1">View More</span>
+          <v-icon color="error">mdi-xml</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </default-layout>
+</template>
+
+<script>
+import DefaultLayout from "../layouts/Default";
+import aboutMeData from "../static/data/about-me.json";
+import ProjectCard from "../components/ProjectCard";
+import RepositoryCard from "../components/RepositoryCard";
+
+export default {
+  components: { RepositoryCard, ProjectCard, DefaultLayout },
+
+  metaInfo: {
+    title: "Hello, world!"
+  },
+
+  data() {
+    return {
+      aboutMe: aboutMeData,
+      slidGroupState: null
+    };
+  }
+};
+</script>
