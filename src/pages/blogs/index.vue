db3e707ca3c8fad13eb904622109d742fc868e16
Basti Lavarias <denzilbao@gmail.com>
2020-06-26 02:51:44 +0800

    [PROGRESS] pages blog folder created.

---
+++ src/pages/blogs/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <default-layout>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <v-card>test</v-card>
+      </v-col>
+    </v-row>
+  </default-layout>
+</template>
+<script>
+import DefaultLayout from "../../layouts/Default";
+export default {
+  components: { DefaultLayout }
+};
+</script>
