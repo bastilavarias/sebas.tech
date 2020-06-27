288ef15eb5dc8d4ff5f006d734f2c9a01df75bd3
Basti Lavarias <denzilbao@gmail.com>
2020-06-27 20:08:01 +0800

    Updates

--- gridsome.server.js
+++
@@ -1,9 +0,0 @@
-const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
-
-module.exports = function (api) {
-  api.chainWebpack((config, { isServer }) => {
-    config.plugin("vuetify-loader").use(VuetifyLoaderPlugin);
-  });
-  api.loadSource(({ addCollection }) => {});
-  api.createPages(({ createPage }) => {});
-};
